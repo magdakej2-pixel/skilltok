d71843cfb17df3679d0f1a0b77eec874004823bb
fkasp <fkasp@users.noreply.github.com>
2026-03-12 19:32:28 +0000

    security-remove-hardcoded-firebase-keys

--- services/firebase.ts
+++ services/firebase.ts
@@ -7,13 +7,12 @@ import { Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBsLW6GINIluKiEbI4X_85AjRK1QbjRHwo',
-  authDomain: 'skilltok-5053f.firebaseapp.com',
-  projectId: 'skilltok-5053f',
-  storageBucket: 'skilltok-5053f.firebasestorage.app',
-  messagingSenderId: '407572820644',
-  appId: '1:407572820644:web:6f3d6d1c4a961455682c31',
-  measurementId: 'G-W48M279K6W',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
